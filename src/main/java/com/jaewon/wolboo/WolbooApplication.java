0b24cd21ff81a2f6e32754a00d9aba337dcfb021
Your Name <you@example.com>
2024-12-14 19:15:18 +0900

    project start

---
+++ src/main/java/com/jaewon/wolboo/WolbooApplication.java
@@ -0,0 +1,13 @@
+package com.jaewon.wolboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolbooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolbooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaewon/wolboo/WolbooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewon.wolboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolbooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
